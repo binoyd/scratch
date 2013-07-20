cf546704fc49124366f274031f4f91fab7460c0f
Binoy <Binoy@192.168.1.9>
2013-07-19 20:08:31 -0700

    Commit to Scratch

---
+++ HelloGitWorld/src/com/scratch/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.scratch.helloworld;
+
+/**
+ * @author Binoy
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+
+	}
+
+}
